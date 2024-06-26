7b46db0e5bbecfad12799ecdd7368c6833c505b4
vital <vgf@umich.edu>
2024-06-26 15:38:21 +0100

    Innate 0.1.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,29 @@ author = 'Vital-Fernandez'
 release = '06/25/2024'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.4'
+
+# -- Moving documentation building data functions ----------------------------
+
+# import sys
+# import os
+# import shutil
+# from pathlib import Path
+#
+# def all_but_ipynb(dir, contents):
+#     result = []
+#     for c in contents:
+#         if os.path.isfile(os.path.join(dir, c)) and (not c.endswith(".py")):
+#             result += [c]
+#     return result
+#
+#
+# _lib_path = Path(__file__).parents[2]/'src'
+# _doc_folder = Path(__file__).parents[2]/'docs/source'
+# _examples_path = Path(__file__).parents[2]/'examples'
+# sys.path.append(_lib_path.as_posix())
+# sys.path.append(_examples_path.as_posix())
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -42,3 +64,12 @@ html_theme = 'sphinx_rtd_theme'
 
 imgmath_latex_preamble = r'\usepackage[active]{preview}' # + other custom stuff for inline math, such as non-default math fonts etc.
 imgmath_use_preview = True
+
+# -- Move the files ------------------------------------------------------------
+
+# shutil.rmtree(_doc_folder/'images', ignore_errors=True)
+# shutil.rmtree(_doc_folder/'inputs', ignore_errors=True)
+# shutil.rmtree(_doc_folder/'outputs', ignore_errors=True)
+# shutil.rmtree(_doc_folder/'sample_data', ignore_errors=True)
+# shutil.rmtree(_doc_folder/'tutorials', ignore_errors=True)
+# shutil.copytree(_examples_path, _doc_folder, dirs_exist_ok=True)
--- examples/tutorials/3_eqn_regression.py
+++ examples/tutorials/3_eqn_regression.py
@@ -1,13 +1,15 @@
 import innate
+import numpy as np
 import pyneb as pn
 
 data_file = '../data/emissivity_grids.nc'
 emissivities = innate.DataSet.from_file(data_file)
 
 temp, den = 12250, 122
-print('Interpolation', emissivities['O3_5007A'].approx.interp.rgi((temp, den)).eval())
+variables = (temp, den)
+print('Regression Equation', emissivities['O3_5007A'].approx.reg.eqn(variables))
 
 # Compare with the original data
 O3, H1 = pn.Atom('O', 3), pn.RecAtom('H', 1)
 emiss_ratio = O3.getEmissivity(temp, den, wave=5007)/H1.getEmissivity(temp, den, wave=4861)
-print('True value', emiss_ratio)
+print('True value', np.log10(emiss_ratio))
