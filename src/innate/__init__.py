213c5296d1970e72b3c759f3385df9f2c6f95098
vital <vgf@umich.edu>
2026-04-30 19:59:04 -0400

    innate 0.2.2

--- src/innate/__init__.py
+++ src/innate/__init__.py
@@ -12,6 +12,6 @@ _conf_path = _inst_dir/'config.toml'
 with open(_conf_path, mode="rb") as fp:
     _setup_cfg = tomllib.load(fp)
 
-from .io import save_dataset
+from .io import load_dataset, save_dataset
 
 from .main import DataSet, Grid
